405a63d1ddc5ad376a460d625d4c3c8b6296acf9
Kyle Fournier <123187531+CompSciKyle@users.noreply.github.com>
2024-12-07 16:33:39 -0500

    Made some changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb'],
-  base: '/'
-
-})
-//Making a subtree gh-pages
+  base: '/', // Correct base path for deployment
+  build: {
+    rollupOptions: {
+      input: './index.html', // Ensure this points to your entry file
+    },
+  },
+});
