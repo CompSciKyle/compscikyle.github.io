98e206f0d984343f65356e8e53b24d1cb5e59e58
Kyle Fournier <123187531+CompSciKyle@users.noreply.github.com>
2024-12-07 16:14:44 -0500

    Updated branch

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  base: '3D-Portfolio'
+  base: '/'
 
 })
 //Making a subtree gh-pages
