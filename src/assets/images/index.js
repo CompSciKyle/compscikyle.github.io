a2ac2fcd12a6cb046da082310b432935cc6e95eb
Kyle Fournier <123187531+CompSciKyle@users.noreply.github.com>
2024-12-07 21:03:09 -0500

    Bug fixes

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,10 +1,14 @@
 import goblin from './goblin.jpg'
 import pools from './pools.png'
 import parhamfair from './parhamfair.png'
+import career from './career.png'
+import calendar from './calendar.png'
 
 
 export {
     goblin,
     pools,
     parhamfair,
+    career,
+    calendar,
 }
